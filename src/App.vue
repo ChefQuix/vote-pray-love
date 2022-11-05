6b253b8364627824c8f643c6c9286adc00c06143
Andrew Burton <chefquix@gmail.com>
2022-11-04 22:05:49 -0500

    axios, router

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
-    <header>
-      <h1>Hello Vote</h1>
-    </header>
-    <main>
-      <p>This is a test</p>
-    </main>
+
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+//const API_URL = `https://data.winnipeg.ca/resource/f9mn-vti8.json`;
 
 export default {
+  data() {
+    committees: [],
+  },
   name: 'App',
+  async created() {
+    let url = `${this.$API_URL}?$select=committee_name,committee&$group=committee_name`
+    let data = await axios.get(url);
+    this.committees = data;
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+const app = createApp(App).mount('#app')
+
+app.provide(`$API_URL`,`https://data.winnipeg.ca/resource/f9mn-vti8.json`);
